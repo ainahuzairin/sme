1056ce54dfe664402e421c7a803a9139c1496205
ainahuzairin <ainahuzairin@gmail.com>
2026-07-22 14:39:06 +0800

    commit

--- sme/obj/Debug/net10.0/sme.AssemblyInfo.cs
+++ sme/obj/Debug/net10.0/sme.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sme")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1c281ce2275b80fa21f57096cce8742f877e932")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0de400edd7c6e9cf009f395739776f45a5f26129")]
 [assembly: System.Reflection.AssemblyProductAttribute("sme")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sme")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
